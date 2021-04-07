ce68511dc3dc4af2bff2b146bcbb08d96822a98f
Andy Stward Lanfranco Ayala <andy_lanfranco@ucne.edu.do>
2021-04-07 17:35:21 -0400

    cambios

--- InternManagement/Models/Institucione.cs
+++ InternManagement/Models/Institucione.cs
@@ -14,17 +14,16 @@ namespace InternManagement.Models
         }
 
         public int InstitucionId { get; set; }
-        [Required(ErrorMessage ="El Campo Nombre no puede estar vacio")]
+
+        [Required(ErrorMessage = "El Campo Nombres no puede estar vacio")]
         public string Nombre { get; set; }
         [Required(ErrorMessage = "El Campo Telefono no puede estar vacio")]
-        [RegularExpression(@"^[01]?[- .]?(\([2-9]\d{2}\)|[2-9]\d{2})[- .]?\d{3}[- .]?\d{4}$", ErrorMessage = "Teléfono no válido.")]
         public string Telefono { get; set; }
         [Required(ErrorMessage = "El Campo Email no puede estar vacio")]
         public string Email { get; set; }
-
         [Required(ErrorMessage = "El Campo Direccion no puede estar vacio")]
         public string Direccion { get; set; }
-        [Required(ErrorMessage = "El Campo Fecha de creacion no puede estar vacio")]
+        
         public DateTime FechaCreacion { get; set; } = DateTime.Now;
 
         public virtual ICollection<Pasante> Pasantes { get; set; }
